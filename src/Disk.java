4055bb8d3040e1f67ee161529f6ac68d3f294ca1
paulo9mv <paulo9mv@yahoo.com.br>
2017-11-17 16:14:20 -0300

    Routine process OK

---
+++ src/Disk.java
@@ -0,0 +1,38 @@
+public class Disk{
+    private LinkedList<Process> list = new LinkedList<Process>();
+    private Process tempProcess;
+    private Despachante despachante;
+    private Random mRandom = new Random();
+
+    public boolean stop = false;
+    public boolean first = true;
+
+    public void newProcessDisk(Process process){
+        list.addLast(process);
+        if(first){
+            tempProcess = process;
+            first = false;
+        }
+    }
+
+    public void processing(){
+        while(!stop){
+            if(!list.isempty()){
+            if(tempProcess.diskComplete()){
+                despachante.receiveBlockedProcess(tempProcess);
+
+                tempProcess = list.removeFirst();
+            }
+
+            if(mRandom.nextInt(100) < 20){  //Random pause on process request I/O
+                despachante.receiveBlockedProcess(tempProcess);
+
+                tempProcess = list.removeFirst();
+            }
+            else
+                tempProcess.setDisk_cycles_to_complete(1);
+        }
+    }
+    }
+
+}
---
+++ src/Printer.java
@@ -0,0 +1,39 @@
+public class Printer{
+    private LinkedList<Process> list = new LinkedList<Process>();
+    private Process tempProcess;
+    private Despachante despachante;
+    private Random mRandom = new Random();
+
+    public boolean stop = false;
+    public boolean first = true;
+
+    public void newProcessPrinter(Process process){
+        list.addLast(process);
+        if(first){
+            tempProcess = process;
+            first = false;
+        }
+    }
+
+    public void processing(){
+        while(!stop){
+            if(!list.isempty()){
+            if(tempProcess.printerComplete()){
+                despachante.receiveBlockedProcess(tempProcess);
+
+                tempProcess = list.removeFirst();
+            }
+
+            if(mRandom.nextInt(100) < 20){  //Random pause on process request I/O
+                despachante.receiveBlockedProcess(tempProcess);
+
+                tempProcess = list.removeFirst();
+            }
+            else
+                tempProcess.setPrinter_cycles_to_complete(1);
+        }
+    }
+    }
+
+
+}
