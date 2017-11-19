165601d81352afb0194e5f82bc776e890c58f912
paulo9mv <paulo9mv@yahoo.com.br>
2017-11-19 18:08:00 -0300

    Core adjust, avoiding conflicts

--- src/Core.java
+++ src/Core.java
@@ -13,7 +13,7 @@ public class Core implements Runnable{
     public final static int DISK = 2;
     public final static int END = 3;
 
-    private Random random = new Random();
+    private Random mRandom = new Random();
 
     private boolean busy = false;
     public boolean stop = false;
@@ -37,10 +37,10 @@ public class Core implements Runnable{
     private void ioBlock(int flag){
         actual_process.setState(Process.BLOCKED);
         despachante.fromCore(actual_process, flag);
+        busy = false;
     }
 
     public void toProcess(Process process){
-       // System.out.println("Core nao ocupado, startando");
         if(!busy){
             actual_process = process;
             busy = true;    
@@ -49,47 +49,34 @@ public class Core implements Runnable{
 
     private void processing(){
         if(busy){
-        actual_process.setState(Process.IN_EXECUTION);
-        
-        if(actual_process.isDone()){
-            System.out.println("Processo pronto, encerrando.");
-            despachante.fromCore(actual_process, END);
-        }
-
-        while(actual_process.getState() == Process.IN_EXECUTION){
-        System.out.printf("CPU Processo %d adicionado! CPU:%d DISK:%d PRINTER:%d\n%d C = %d D = %d P = %d\n", actual_process.getId(), actual_process.getcycles_processed(), actual_process.getDisk_cycles_processed(),
-                actual_process.getPrinter_cycles_processed(), actual_process.getId(),actual_process.getcycles_to_complete(), actual_process.getDisk_cycles_to_complete(), actual_process.getPrinter_cycles_to_complete());
-        if(actual_process.isDone()){
-            System.out.printf("Processo pronto! Saindo CPU\n");
-            break;
-        }
-        if(random.nextInt(100) < 20){
-            if(actual_process.hasDisk() && !actual_process.diskComplete())
-                ioBlock(DISK);
-        }
-        else if(random.nextInt(100) < 20){
-            if(actual_process.hasPrinter() && !actual_process.printerComplete())
-                ioBlock(PRINTER);
-        }
-        else if(!actual_process.CPUComplete()){
+            actual_process.setState(Process.IN_EXECUTION);
             
-            actual_process.setcycles_processed(1);
-            System.out.printf("CPU executou. Processo %d. Atual %d\n",actual_process.getId(), actual_process.getcycles_processed());
-        }
-        else{
-            if(actual_process.hasPrinter() && !actual_process.printerComplete()){
-                ioBlock(PRINTER);
+            while(actual_process.getState() == Process.IN_EXECUTION){
+            if(actual_process.isDone())
+                ioBlock(Core.END);
+            if(!actual_process.CPUComplete()){
+                actual_process.setcycles_processed(1);
             }
-            else if(actual_process.hasDisk() && !actual_process.diskComplete()){
-                ioBlock(PRINTER);
+            if(mRandom.nextInt(100) < 20 && !actual_process.printerComplete()){
+                this.ioBlock(Core.PRINTER);
+                break;
             }
-        }
-        
-        }
-        if(actual_process.isDone())
-            despachante.fromCore(actual_process, END);
-
-        busy = false;
+            else if(mRandom.nextInt(100) < 20 && !actual_process.diskComplete()){
+                this.ioBlock(Core.DISK);
+                break;
+            }
+            if(actual_process.CPUComplete()){
+                if(!actual_process.diskComplete()){
+                    ioBlock(Core.DISK);
+                    break;
+                }
+                else if(!actual_process.printerComplete()){
+                    ioBlock(Core.PRINTER);
+                    break;
+                }
+            }
+            
+            }
         }
     }
 
